fd6104b7d3b9d3093c373d62dae28c37f8dc5ce3
Ajay Rajput <ajayhbti11@gmail.com>
2019-05-31 11:18:28 +0530

    fromIterable

--- app/src/main/java/com/ajayraj/rxjavademo/MainActivity.java
+++ app/src/main/java/com/ajayraj/rxjavademo/MainActivity.java
@@ -2,12 +2,65 @@ package com.ajayraj.rxjavademo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ajayraj.rxjavademo.data.DataSource;
+import com.ajayraj.rxjavademo.model.Task;
+
+
+
+
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
+private static  final String TAG=MainActivity.class.getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inIt();
     }
+
+    private  void inIt(){
+
+        Observable<Task> taskObservable = Observable // create a new Observable object
+                .fromIterable(DataSource.createTasksList()) // apply 'fromIterable' operator
+                .subscribeOn(Schedulers.io()) // designate worker thread (background)
+                .observeOn(AndroidSchedulers.mainThread()); // designate observer thread (main thread)
+
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onNext: : " + d.toString());
+
+            }
+
+            @Override
+            public void onNext(Task task) {
+                Log.d(TAG, "onNext: : " + task.getDescription());
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onNext: : " + e.toString());
+
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: : " );
+
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/ajayraj/rxjavademo/data/DataSource.java
@@ -0,0 +1,24 @@
+package com.ajayraj.rxjavademo.data;
+
+import com.ajayraj.rxjavademo.model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * apttrix.com
+ * Created by Ajay Raj on 31,May,2019
+ */
+
+public class DataSource {
+
+    public static List<Task> createTasksList(){
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("Take out the trash", true, 3));
+        tasks.add(new Task("Walk the dog", false, 2));
+        tasks.add(new Task("Make my bed", true, 1));
+        tasks.add(new Task("Unload the dishwasher", false, 0));
+        tasks.add(new Task("Make dinner", true, 5));
+        return tasks;
+    }
+}
